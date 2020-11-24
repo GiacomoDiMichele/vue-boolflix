6edfdb20016d3da96304bab9155103338e26a7cd
Giacomo <giacomo.1936@gmail.com>
2020-11-24 13:04:44 +0100

    aggiunta barra di ricerca, header e struttura vue

---
+++ main.js
@@ -0,0 +1,16 @@
+var app = new Vue ({
+    el: '#root',
+    data: {
+
+    },
+    methods: {
+
+    },
+    mounted() {
+        const self = this;
+        axios.get()
+            .then(function(film) {
+
+            });
+    }
+});
