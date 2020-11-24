62329d19c86bf4c89ec5dd55b8e6284bc4f0f6b6
Giacomo <giacomo.1936@gmail.com>
2020-11-24 15:45:28 +0100

    aggiunto test api

--- main.js
+++ main.js
@@ -8,9 +8,13 @@ var app = new Vue ({
     },
     mounted() {
         const self = this;
-        axios.get()
-            .then(function(film) {
-
-            });
+        axios.get('https://api.themoviedb.org/3/search/movie',{
+                params: {
+                    api_key: 'ef9ceb72102dc577a5c353dc8d030bdc',
+                    query: 'marco'
+                }
+            }).then(function(films) {
+                console.log(films)
+        });
     }
 });
