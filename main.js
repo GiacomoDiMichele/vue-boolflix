b694808297ed122fbd786a6b88276e204cc1fd5e
Giacomo <giacomo.1936@gmail.com>
2020-11-24 16:38:27 +0100

    test film in pagina tramite api

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 var app = new Vue ({
     el: '#root',
     data: {
+        list_films: [
 
+        ]
     },
     methods: {
+        researchFilms() {
 
+        }
     },
     mounted() {
-        const self = this;
-        axios.get('https://api.themoviedb.org/3/search/movie',{
+        axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
                     api_key: 'ef9ceb72102dc577a5c353dc8d030bdc',
                     query: 'marco'
                 }
-            }).then(function(films) {
-                console.log(films)
-        });
-    }
-});
+            }).then((films) => {
+                this.list_films = films.data.results;
+                console.log(this.list_films);
+            });
+        }
+    });
