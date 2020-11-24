4d8446fe30bb0903bffd07aeba4ac874cafb020e
Giacomo <giacomo.1936@gmail.com>
2020-11-24 17:39:03 +0100

    fine milestone 1, inserita funzione di ricerca film

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 var app = new Vue ({
     el: '#root',
     data: {
+        //creo la chiave search_films vuota dove andrò ad inserire il valore che ho inserito nellinput, dando un v-model
+        search_films:'',
+        //creo la lista film dove andrò ad inserire tutti i risultati(dati) dei film
         list_films: [
 
         ]
     },
     methods: {
-        researchFilms() {
-
-        }
-    },
-    mounted() {
-        axios.get('https://api.themoviedb.org/3/search/movie', {
-                params: {
-                    api_key: 'ef9ceb72102dc577a5c353dc8d030bdc',
-                    query: 'marco'
-                }
-            }).then((films) => {
-                this.list_films = films.data.results;
-                console.log(this.list_films);
-            });
+        //creo una funzione al click che mi andrà a cambiare dinamicamente, attraverso il valore di v-model, la query, e quindi il risultato dei film visualizzato in pagina
+        searchTypeFilms() {
+            //se scriverò qualcosa e non lascerò la barra vuota cambio il parametro della query
+             if(this.search_films != '') {
+               axios.get('https://api.themoviedb.org/3/search/movie', {
+                 params: {
+                   api_key: 'ef9ceb72102dc577a5c353dc8d030bdc',
+                   query: this.search_films,
+                 }
+            //prendo tutta l'api con i dati dei film
+             }).then((films) => {
+                 //pusho o assegno nella lista film tutti i risultati(dati) dell'api
+                 this.list_films = films.data.results;
+                 console.log(this.list_films);
+               });
+             }
+            }
         }
     });
