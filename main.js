fa91fd552ffe7de86cd5f51ac285d733c5d39d2c
Giacomo <giacomo.1936@gmail.com>
2020-11-25 22:19:04 +0100

    aggiunta bandiere per lingua film // aggiunta chiamata api per le serie

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var app = new Vue ({
         //creo la lista film dove andrò ad inserire tutti i risultati(dati) dei film
         list_films: [
 
+        ],
+        list_tv: [
+
         ]
     },
     methods: {
@@ -22,15 +25,29 @@ var app = new Vue ({
              }).then((films) => {
                  //pusho o assegno nella lista film tutti i risultati(dati) dell'api
                  this.list_films = films.data.results;
-                 console.log(this.list_films);
-               });
-               this.search_films = '';
-             }
-         },
+                });
+               //aggancio la ricerca delle serie tv tramite una nuova api, in modo che la funzione cerchi sia nell'api film che serie tv, applicando gli stessi parametri
+               axios.get('https://api.themoviedb.org/3/search/tv', {
+                   params: {
+                       api_key: 'ef9ceb72102dc577a5c353dc8d030bdc',
+                       query: this.search_films,
+                   }
+              }).then((films) => {
+                  //assegno i dati dei risultati dell'api serie tv al nuovo array, che conterrà solo i dati serie tv
+                   this.list_tv = films.data.results;
+                });
+            }
+         //se cercherò di far partire la ricerca senza aver scritto nulla farò apparire un avviso di mancato inserimento del testo nella barra di ricerca
+         else {
+             alert('inserisci testo nella barra di ricerca')
+         }
+     },
+
         avgVoteStar(films) {
             let number_vote = Math.round(films.vote_average / 2);
                 return number_vote
         },
+
         emptyStar(films) {
             let rest_vote = 10 - films.vote_average;
             let number_rest_vote = Math.round(rest_vote / 2);
